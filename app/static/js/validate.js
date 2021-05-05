9910b8eb73ce4e29d6a6bcb29069132c2b9015b8
Tripti Lamba <tripti.lamba18@st.niituniversity.in>
2021-05-05 14:45:35 +0530

    Updates

--- app/static/js/validate.js
+++ app/static/js/validate.js
@@ -4,7 +4,7 @@ const adults = document.getElementById('adults')
 const locations = document.getElementById('locations')
 
 $(document).ready(function () {
-    $("#form_enlist").on('submit', function (e) {
+    $("#form_search").on('submit', function (e) {
         e.preventDefault();
     });
 });
@@ -22,54 +22,74 @@ function validate_homeform() {
 
     if (date_arrival.value == "") {
         printError("ArDateErr", "Arrival date can't be empty");
-    }
+    } else {
+            printError("ArDateErr", "");
+            ArDateErr = false;
+        }
 
     if (date_departure.value == "") {
         printError("DeDateErr", "Departure date can't be empty");
-    }
+    } else {
+            printError("DeDateErr", "");
+            DeDateErr = false;
+        }
 
     if (locations.value == "") {
         printError("LocErr", "Departure date can't be empty");
-    }
+    } else {
+            printError("LocErr", "");
+            LocErr = false;
+        }
 
 
     if ((Date.parse(date_arrival) >= Date.parse(date_departure))) {
         printError("DateErr", "Departure date can't be lesser than arrival date");
 
-    }
+    } else {
+            printError("DateErr", "");
+            DateErr = false;
+        }
     if (adults.value() == null) {
         printError("AdultErr", "No. of guests can't be empty");
+    } else {
+            printError("AdultErr", "");
+            AdultErr = false;
+        }
+
+    if (DateErr || ArDateErr || DeDateErr || LocErr || AdultErr == true) {
+        return false;
+    } else {
+        return true;
     }
-
-    if (alerts == []){
-         return('Validated')
-    }
-    else {
-        return (alerts.values())
-    }
-
-
-    return((document.write(alerts[0], alerts[1])))
-
 }
 
-function home_valcall(){  return(
-        validate_homeform()
-        &&
-            $.ajax({
-            type:'POST',
-            url : '/',
-            data:{
-                location: $('#location').val(),
-                type: $('#type').val(),
-				from: $('#from').val(),
-				to: $('#to').val(),
-				guest: $('#guest').val()
-            }},
+function homeValCall() {
+    const myform = document.getElementById('form_search');
+    const formData = new FormData(myform);
 
-    ));
+
+    $.ajax({
+        type: 'POST',
+        url: '/',                 //add url idhar
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        enctype: 'multipart/form-data',
+        contentType: false,
+        async: !1,
+        cache: !1,
+        success: function () {
+            alert('form was submitted');
+        }
+
+    });
 }
 
+function submitFormhome() {
+    validate_homeform()
+    alert(validate_homeform())
+
+}
 
 
 
