b0c68fc3e99d348d32f3fcc1904e7e574ab07013
kanishya <kanishyamohan@gmail.com>
2021-05-08 18:11:46 +0530

    updated header

--- app/views.py
+++ app/views.py
@@ -344,12 +344,12 @@ def manageProperty():
 
 @app.route('/about')
 def about():
-    return render_template('space_gallery.html')
+    return render_template('about.html')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('testing.html')
+    return render_template('contact.html')
 
 
 @app.route('/property/<token>', methods=['get', 'post'])
